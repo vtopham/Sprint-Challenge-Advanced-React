b10016c1cee463f59d665f045a931ef03a3ecd6a
Victoria  Topham <topham.victoria@gmail.com>
2020-04-10 08:12:32 -0700

    fetched data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+
 
 class App extends React.Component{
   constructor() {
     super()
     this.state = {
-
+      playersArray: []
+      //SHAPE: array of objects with country, id, name, searches
     }
+    
   }
+
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/players")
+    .then(response => {
+      this.setState({
+        playersArray:response.data
+      })
+    })
+  }
+  
 
   render() {
     return(
